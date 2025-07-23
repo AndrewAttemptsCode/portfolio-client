c4ab93d3d25e864f73bc84c89f71d0e314f736f5
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-07-23 18:03:28 +0100

    feat: add burger menu that responds to is open state

---
+++ src/components/BurgerMenu.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  position: absolute;
+  left: 0;
+  transform: translateY(-50%);
+  height: 2rem;
+  aspect-ratio: 1 / 1;
+  padding: 0.1rem;
+  background: transparent;
+  border: 2px solid var(--primary-font-color);
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+`
+
+const ButtonContent = styled.div`
+  position: relative;
+  user-select: none;
+  height: 2px;
+  background: ${({$open}) => ($open ? "transparent" : "var(--primary-font-color)")}; 
+  transition: background 0.3 ease;
+
+  &::before,
+  &::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    user-select: none;
+    height: 2px;
+    width: 100%;
+    background: var(--primary-font-color);
+    transition: transform 0.3s ease;
+  }
+
+  &::before {
+    transform: ${({$open}) => ($open ? "rotate(45deg)" : "translateY(-6px)")};
+  }
+
+  &::after {
+    transform: ${({$open}) => ($open ? "rotate(-45deg)" : "translateY(6px)")};
+  }
+`
+
+const BurgerMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <StyledButton
+      onClick={() => setIsOpen(prev => !prev)}
+      aria-label="Toggle Navigation"
+      aria-expanded={isOpen}
+    >
+      <ButtonContent $open={isOpen} />
+    </StyledButton>
+  );
+};
+
+export default BurgerMenu;
