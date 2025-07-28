94bd609e5eafed7cf5cd9568d1851528a758cf34
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-07-28 13:58:35 +0100

    feat: add about me call to action component to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
+import AboutCTA from "../components/AboutCTA";
 import HeroSection from "../components/HeroSection"
 
 const HomePage = () => {
   return (
     <>
       <HeroSection />
+      <AboutCTA />
     </>
   );
 };
