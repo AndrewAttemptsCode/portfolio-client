4f70d93570ff21e4a8005cf5fff99c7beae25b45
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-07-29 19:01:18 +0100

    feat: add component that renders tech stack by groups

---
+++ src/components/TechStackGroup.jsx
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+import techstacklist from "../utils/techstacklist";
+
+const Container = styled.div`
+  padding: 1rem 0;
+`
+
+const StackContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+  overflow-x: auto;
+`
+
+const ItemContainer = styled.a`
+  aspect-ratio: 1 / 1;
+  height: 7.75rem;
+  color: inherit;
+  text-decoration: none;
+  padding: 1rem;
+  background-color: rgba(var(--primary-color), 0.06);
+  text-align: center;
+  line-height: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
+
+const IconContainer = styled.div`
+  aspect-ratio: 1 / 1;
+  height: 4rem;
+  margin: 0 auto;
+
+  svg {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+`
+
+const TechStackGroup = ({ categoryTitle, category }) => {
+  const list = techstacklist[category];
+
+  return (
+    <Container>
+      <h3>{categoryTitle}</h3>
+      <StackContainer>
+        {list.map(({ title, url, icon }) => (
+          <ItemContainer key={title} href={url} target="_blank" rel="noopener noreferrer">
+            <IconContainer aria-hidden="true">
+              {icon}
+            </IconContainer>
+            <p>{title}</p>
+          </ItemContainer>
+        ))}
+      </StackContainer>
+    </Container>
+  );
+};
+
+export default TechStackGroup;
