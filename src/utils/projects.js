e8e72fe8e335711e64fceb85cbffd8cdbd8c25a5
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-07-30 19:58:01 +0100

    feat: add initial projects list

---
+++ src/utils/projects.js
@@ -0,0 +1,21 @@
+const projects = {
+  featured: [
+    {
+      title: "Chat App",
+      // image: image preview here,
+      links: {
+        demo: ["https://www.google.co.uk", "https://www.twitch.tv"],
+        source: ["https://www.google.co.uk", "https://www.twitch.tv"],
+      },
+      description: ["Lorem, ipsum dolor sit amet consectetur adipisicing elit.", "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus saepe cumque."],
+      techstack: {
+        frontend: ["HTML", "CSS"],
+        backend: ["Node.js"],
+        tools: ["VS Code"],
+        testing: ["SuperTest"],
+      },
+    },
+  ],
+};
+
+export default projects;
