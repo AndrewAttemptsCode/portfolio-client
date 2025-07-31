2d13a088e6ac3d0e0c1105647a0a2eac4d394154
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-07-31 18:51:35 +0100

    feat: add with filler featured projects

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -15,6 +15,36 @@ const projects = {
         testing: ["SuperTest"],
       },
     },
+    {
+      title: "Second App",
+      // image: image preview here,
+      links: {
+        demo: ["https://www.google.co.uk", "https://www.twitch.tv"],
+        source: ["https://www.google.co.uk", "https://www.twitch.tv"],
+      },
+      description: ["Lorem, ipsum dolor sit amet consectetur adipisicing elit.", "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus saepe cumque."],
+      techstack: {
+        frontend: ["HTML", "CSS"],
+        backend: ["Node.js"],
+        tools: ["VS Code"],
+        testing: ["SuperTest"],
+      },
+    },
+    {
+      title: "Third App",
+      // image: image preview here,
+      links: {
+        demo: ["https://www.google.co.uk", "https://www.twitch.tv"],
+        source: ["https://www.google.co.uk", "https://www.twitch.tv"],
+      },
+      description: ["Lorem, ipsum dolor sit amet consectetur adipisicing elit.", "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus saepe cumque."],
+      techstack: {
+        frontend: ["HTML", "CSS"],
+        backend: ["Node.js"],
+        tools: ["VS Code"],
+        testing: ["SuperTest"],
+      },
+    },
   ],
 };
 
