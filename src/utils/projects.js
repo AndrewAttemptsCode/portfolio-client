eed9637b154bb4bc638c931921655d3ead388ee8
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-08-02 18:27:38 +0100

    feat: update first featured project with project details

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -8,15 +8,15 @@ const projects = {
       title: "Messenger Chat App",
       image: messengerApp,
       links: {
-        demo: ["https://www.google.co.uk", "https://www.twitch.tv"],
-        source: ["https://www.google.co.uk", "https://www.twitch.tv"],
+        demo: ["https://odin-messaging-app-client.pages.dev/"],
+        source: ["https://github.com/AndrewAttemptsCode/odin-messaging-app-client", "https://github.com/AndrewAttemptsCode/odin-messaging-app-server"],
       },
       description: ["Lorem, ipsum dolor sit amet consectetur adipisicing elit.", "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus saepe cumque."],
       techstack: {
-        frontend: ["HTML", "CSS"],
-        backend: ["Node.js"],
-        tools: ["VS Code"],
-        testing: ["SuperTest"],
+        frontend: ["HTML", "CSS", "JavaScript", "React", "Styled Components", "Vite"],
+        backend: ["Node.js", "Express", "PostgreSQL", "Prisma", "Passport.js"],
+        tools: ["Git", "GitHub", "VS Code", "Railway", "Cloudflare Pages", "EsLint", "Prettier"],
+        testing: [],
       },
     },
     {
