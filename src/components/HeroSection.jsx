5796835bf10d6cf171341d58f03f85909efdd0ef
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-08-07 17:51:00 +0100

    style: update hero section height and layout to reflect hero banner styling

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,12 +4,20 @@ import SocialsLinks from "./SocialsLinks";
 const HeroStyle = styled.section`
   padding: 1rem 0;
   background-color: rgba(var(--primary-color), 0.06);
+  min-height: 30vh;
+  min-height: 30dvh;
+
 `
 
 const ResponsiveContainer = styled.div`
   width: min(80%, 1280px);
   margin: 0 auto;
   text-align: center;
+  min-height: inherit;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
   h2 {
     line-height: 1;
