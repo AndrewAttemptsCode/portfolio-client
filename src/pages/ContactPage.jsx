7a8b5b8184eda3e384196f099eb1aa1086e3c06c
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-08-11 17:53:41 +0100

    feat: add herobanner, contactform and social links components

---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import ContactForm from "../components/ContactForm";
+import HeroBanner from "../components/HeroBanner";
+import SocialsLinks from "../components/SocialsLinks";
+
+const Container = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 2rem;
+  margin-bottom: 1rem;
+`
+
+const ContactPage = () => {
+  return (
+    <>
+      <HeroBanner title="Contact" subtitle="Something peaked your interest? ...send me a message!"/>
+      <Container>
+        <ContactForm />
+        <SocialsLinks />
+      </Container>
+    </>
+  );
+};
+
+export default ContactPage;
