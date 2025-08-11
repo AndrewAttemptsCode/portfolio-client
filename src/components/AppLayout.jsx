c62d37c1c1cf3d814f1c816d42e4ac1c0bffa261
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-08-11 17:52:57 +0100

    feat: update app layout container to display flex and main outlet to flex to fill space

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,16 +1,30 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  min-height: 100dvh;
+`
+
+const Main = styled.main`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+`
 
 const AppLayout = () => {
   return (
-    <>
+    <Container>
       <Header />
-      <main>
+      <Main>
         <Outlet />
-      </main>
+      </Main>
       <Footer />
-    </>
+    </Container>
   );
 };
 
